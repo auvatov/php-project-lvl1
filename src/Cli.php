5d1d4b09f1a07a59bd0390b6356f444c5b16d5e3
Andrey Uvatov <auvatov@gmail.com>
2022-08-20 15:17:35 +0800

    Step four complete

--- src/Cli.php
+++ src/Cli.php
@@ -1,11 +1,13 @@
 <?php
 
 namespace BrainGames\Cli;
-require_once __DIR__ . '/../vendor/autoload.php';
+
 use function cli\line;
 use function cli\prompt;
 
-function acquaintance() {
+function acquaintance()
+{
+    require_once __DIR__ . '/../vendor/autoload.php';
     line('Welcome to the Brain Game!');
     $name = prompt('May I have your name?');
     line("Hello, %s!", $name);
