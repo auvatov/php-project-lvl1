e6c8726d30bd1c47ce6a7439764da036740fe7b5
Andrey Uvatov <auvatov@gmail.com>
2022-08-24 07:58:58 +0800

    Step five point five

--- src/Cli.php
+++ src/Cli.php
@@ -7,7 +7,7 @@ use function cli\prompt;
 
 function acquaintance()
 {
-    require_once __DIR__ . '/../vendor/autoload.php';
+//    require_once __DIR__ . '/../vendor/autoload.php';
     line('Welcome to the Brain Game!');
     $name = prompt('May I have your name?');
     line("Hello, %s!", $name);
---
+++ src/Even.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace BrainGames\Even;
+
+// require_once __DIR__ . '/../vendor/autoload.php';
+
+use function cli\line;
+use function cli\prompt;
+
+function brainEven()
+{
+    line('Welcome to the Brain Game!');
+    $name = prompt('May I have your name?');
+    line("Hello, %s!", $name);
+    line('Answer "yes" if the number is even, otherwise answer "no".');
+    $roundCount = 3;
+    while ($roundCount > 0) {
+        $num = mt_rand(0, 100);
+        line("Question: %s", $num);
+        $answer = prompt("Your answer");
+        $parity = ($num % 2 === 0) ? "yes" : "no";
+        if ($answer === $parity) {
+            line('Correct!');
+        } else {
+            line("'{$answer}' is wrong answer ;(. Correct answer was '{$parity}'.");
+            line("Let's try again, %s!", $name);
+            return;
+        }
+        $roundCount--;
+    }
+    line("Congratilations, %s!", $name);
+}
