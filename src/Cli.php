3ffda06547fdc6d89ba92bcd6036544acd0fc5ff
Andrey Uvatov <auvatov@gmail.com>
2022-08-29 11:32:18 +0800

    fix Cli.php

--- src/Cli.php
+++ src/Cli.php
@@ -8,7 +8,7 @@ use function cli\prompt;
 function acquaintance()
 {
 //    require_once __DIR__ . '/../vendor/autoload.php';
-    line('Welcome to the Brain Game!');
+    line('Welcome to the Brain Games!');
     $name = prompt('May I have your name?');
     line("Hello, %s!", $name);
 }
