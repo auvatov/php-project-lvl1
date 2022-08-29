a2e209ac7dd38c63824e94a6370f8e23399815da
Andrey Uvatov <auvatov@gmail.com>
2022-08-29 09:41:12 +0800

    add forgotten files

---
+++ src/Games/BCD.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace BrainGames\Games\BCD;
+
+use function cli\line;
+use function cli\prompt;
+use function BrainGames\Engine\acquaintance;
+use function BrainGames\Engine\compare;
+
+use const BrainGames\Engine\ROUNDS;
+
+function getQuestionAndAnswer(): array
+{
+    $num1 = mt_rand(0, 100);
+    $num2 = mt_rand(0, 100);
+    $question = "{$num1} {$num2}";
+    $answer = null;
+    if ($num1 < $num2) {
+        $lesser = $num1;
+        $greater = $num2;
+    } elseif ($num2 < $num1) {
+        $lesser = $num2;
+        $greater = $num1;
+    } else {
+        $answer = $num1;
+    }
+    for ($i = 1; !$answer and $i <= $lesser / 2; $i++) {
+        $divider = $lesser / $i;
+        if ($divider == round($divider) and $greater % $divider === 0) {
+            $answer = $divider;
+        }
+    }
+    if ($answer === null) {
+        $answer = 1;
+    }
+    return [$question, $answer];
+}
+
+function brainBcd()
+{
+    $name = acquaintance();
+    line("Find the greatest common divisor of given numbers.");
+    $roundCount = ROUNDS;
+    while ($roundCount > 0) {
+        [$question, $answer] = getQuestionAndAnswer();
+        if (!compare($question, $answer)) {
+            line("Let's try again, %s!", $name);
+            return;
+        }
+        $roundCount--;
+    }
+    line("Congratilations, %s!", $name);
+}
---
+++ src/Games/Progression.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace BrainGames\Games\Progression;
+
+use function cli\line;
+use function cli\prompt;
+use function BrainGames\Engine\acquaintance;
+use function BrainGames\Engine\compare;
+
+use const BrainGames\Engine\ROUNDS;
+
+function getQuestionAndAnswer(): array
+{
+    $progArray = [];
+    $element = mt_rand(-100, 100); //first element
+    $stepProgr = mt_rand(-10, 10);
+    $progArray[] = $element;
+
+    for ($count = 1; $count < 10; $count++) {
+        $element += $stepProgr;
+        $progArray[] = $element;
+    }
+
+    $hideElementIndex = array_rand($progArray, 1);
+    $answer = $progArray[$hideElementIndex];
+    $progArray[$hideElementIndex] = "..";
+    $question = implode(" ", $progArray);
+
+    return [$question, $answer];
+}
+
+function brainProg()
+{
+    $name = acquaintance();
+    line("What number is missing in the progression?");
+    $roundCount = ROUNDS;
+    while ($roundCount > 0) {
+        [$question, $answer] = getQuestionAndAnswer();
+        if (!compare($question, $answer)) {
+            line("Let's try again, %s!", $name);
+            return;
+        }
+        $roundCount--;
+    }
+    line("Congratilations, %s!", $name);
+}
